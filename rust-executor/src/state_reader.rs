ebc0b3c8405da30d88a5ee156f31bf041ffb1ad8
Dtwosam <42391449+Dtwosam@users.noreply.github.com>
2026-09-25 16:17:08 +0100

    Use Solana 2.3 account encoding type

--- rust-executor/src/state_reader.rs
+++ rust-executor/src/state_reader.rs
@@ -4,10 +4,10 @@ use std::str::FromStr;
 use anchor_client::solana_client::nonblocking::rpc_client::RpcClient;
 use anchor_client::solana_sdk::pubkey::Pubkey;
 use anchor_lang::Discriminator;
+use solana_account_decoder_client_types::UiAccountEncoding;
 use solana_client::rpc_config::{
     RpcAccountInfoConfig, RpcProgramAccountsConfig,
 };
-use solana_client::rpc_response::UiAccountEncoding;
 use solana_client::rpc_filter::{Memcmp, RpcFilterType};
 use anchor_client::solana_sdk::sysvar::clock::{Clock, ID as CLOCK_ID};
 use anyhow::{Context, Result};
